e55e3a0e771121734f7401f8b396e10d8cef9fb8
blackletum <blackletum@hotmail.com>
2025-04-23 14:23:24 -0400

    Update cpu_posix.cpp
    sysctl is deprecated on linux so just surrounding the code to only use it if the system is defined as apple or freebsd

--- src/tier0/cpu_posix.cpp
+++ src/tier0/cpu_posix.cpp
@@ -7,7 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+
+#if defined(__APPLE__) || defined(__FreeBSD__)
 #include <sys/sysctl.h>
+#endif
+
 #include <sys/time.h>
 #include <unistd.h>
 #include <tier0/platform.h>
