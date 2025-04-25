3426539e0df945f67785979a4273933071ccc137
blackletum <blackletum@hotmail.com>
2025-04-25 08:14:02 -0400

    Update utlbufferutil.h

--- src/public/tier1/utlbufferutil.h
+++ src/public/tier1/utlbufferutil.h
@@ -62,6 +62,10 @@ inline bool Unserialize(CUtlBuffer &buf, DmElementHandle_t &dest)
     dest = static_cast<DmElementHandle_t>(temp);
     return true;
 }
+inline bool Serialize(CUtlBuffer& buf, const DmElementHandle_t& src)
+{
+    return Serialize(buf, static_cast<const int&>(src));
+}
 
 //-----------------------------------------------------------------------------
 // Support for DmElementHandle_t (assumed to be int-compatible)
